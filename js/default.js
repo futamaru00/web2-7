68666d2a1d4ff9a356adbc40b7545d36caf3cb98
futamaru00 <38421140+futamaru00@users.noreply.github.com>
2018-11-12 17:45:44 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,10 +1,15 @@
-var number=Math.floor(Math.random()*100);
-var answer=parseInt(window.prompt('数当てゲーム。0～99の数字を入力！'));
+var number = Math.floor(Math.random()*100);
+var answer;
 
 var message;
 
+for(var i = 1; i > 0; i++){
+  
+answer = parseInt(window.prompt('数当てゲーム。0～99の数字を入力！'));
+
 if(answer ===number){
   message = 'あたり！';
+  break;
 }else if(answer < number){
   message = '残念！もっと大きいです';
 }else if(number < answer){
@@ -14,3 +19,4 @@ if(answer ===number){
 }
 
 document.getElementById('choice').textContent = message;
+}
